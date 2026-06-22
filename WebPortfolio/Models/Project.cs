7786ab205e244aeff034116496019166c41e53cb
EricCatayas <catayasericjay@gmail.com>
2026-06-22 09:17:02 +0800

    feat: personal project model

---
+++ WebPortfolio/Models/Project.cs
@@ -0,0 +1,13 @@
+﻿namespace WebPortfolio.Models
+{
+    public class Project
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string? Link { get; set; }
+        public string? GithubLink { get; set; }
+        public string ImageUrl { get; set; }
+        public string ThumbnailUrl { get; set; }
+    }
+}
