eb9d3d2ac387bfa07ed9a5b149f56c8f17814b8b
EricCatayas <catayasericjay@gmail.com>
2026-06-22 09:18:47 +0800

    feat: work timeline page

---
+++ WebPortfolio/Controllers/WorkController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebPortfolio.Data;
+using WebPortfolio.Models;
+
+namespace WebPortfolio.Controllers
+{
+
+    public class WorkController : Controller
+    {
+        [Route("/work")]
+        public IActionResult Index()
+        {
+            var workExperiences = WorkExperienceData.Experiences;
+            return View(workExperiences);
+        }
+        [Route("/work/{id}")]
+        public IActionResult Details(string id)
+        {
+            var workExperiences = WorkExperienceData.Experiences;
+            var workExperience = workExperiences.FirstOrDefault(w => w.Id == id);
+            if (workExperience == null)
+            {
+                return NotFound();
+            }
+            return View(workExperience);
+        }
+    }
+}
