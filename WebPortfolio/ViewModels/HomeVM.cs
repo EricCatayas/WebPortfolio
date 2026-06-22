20ac83e8bddb78e1bfb711c41cd1fff191e1c66a
EricCatayas <catayasericjay@gmail.com>
2026-06-22 09:26:36 +0800

    feat: home view model

---
+++ WebPortfolio/ViewModels/HomeVM.cs
@@ -0,0 +1,10 @@
+﻿using WebPortfolio.Models;
+
+namespace WebPortfolio.ViewModels
+{
+    public class HomeVM
+    {
+        public List<ProjectVM> Projects { get; set; }
+        public List<WorkExperience> WorkExperiences { get; set; }
+    }
+}
