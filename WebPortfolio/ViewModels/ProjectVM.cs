aca75596bc99f7e92e2c7e9d0767389e03dd227b
EricCatayas <catayasericjay@gmail.com>
2026-06-22 09:17:36 +0800

    feat: project view model

---
+++ WebPortfolio/ViewModels/ProjectVM.cs
@@ -0,0 +1,20 @@
+﻿using WebPortfolio.Models;
+
+namespace WebPortfolio.ViewModels
+{
+    public class ProjectVM : Project
+    {
+        public ProjectVM(Project project, string style)
+        {
+            Id = project.Id;
+            Title = project.Title;
+            Description = project.Description;
+            Link = project.Link;
+            GithubLink = project.GithubLink;
+            ImageUrl = project.ImageUrl;
+            ThumbnailUrl = project.ThumbnailUrl;
+            Style = style;
+        }
+        public string Style { get; set; }
+    }
+}
