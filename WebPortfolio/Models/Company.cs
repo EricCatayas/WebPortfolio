2547f727abee04b8321409ebfbc7ba23fc7323a3
EricCatayas <catayasericjay@gmail.com>
2026-06-22 09:16:48 +0800

    feat: work experience and company model

---
+++ WebPortfolio/Models/Company.cs
@@ -0,0 +1,9 @@
+﻿namespace WebPortfolio.Models
+{
+    public class Company
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string Link { get; set; }
+    }
+}
---
+++ WebPortfolio/Models/WorkExperience.cs
@@ -0,0 +1,16 @@
+﻿namespace WebPortfolio.Models
+{
+    public class WorkExperience
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Date { get; set; }
+        public string Type { get; set; }
+        public Company Company { get; set; }
+        public string Link { get; set; }
+        public string ImageUrl { get; set; }
+        public List<string> Contributions { get; set; }
+        public List<string> Technologies { get; set; }
+    }
+}
