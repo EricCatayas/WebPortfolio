2d0770f2e5a45e1d72923e280da95eb5cbee7e5a
EricCatayas <catayasericjay@gmail.com>
2026-06-22 09:25:46 +0800

    feat: projects page

---
+++ WebPortfolio/Controllers/ProjectsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebPortfolio.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        [Route("/projects")]
+        public IActionResult Index()
+        {
+            var projects = Data.ProjectData.Projects;
+
+            return View(projects);
+        }
+    }
+}
