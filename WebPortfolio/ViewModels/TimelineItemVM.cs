385b303645b2845b436282a947fddbe8338671fe
EricCatayas <catayasericjay@gmail.com>
2026-06-22 09:25:24 +0800

    feat: timeline shared view

---
+++ WebPortfolio/ViewModels/TimelineItemVM.cs
@@ -0,0 +1,10 @@
+﻿using WebPortfolio.Models;
+
+namespace WebPortfolio.ViewModels
+{
+    public class TimelineItemVM
+    {
+        public WorkExperience Experience { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
